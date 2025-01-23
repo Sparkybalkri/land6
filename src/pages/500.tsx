847d56868525c77928450e4294f413dd9a9a3c02
Кирилл <52201353+Sparkybalkri@users.noreply.github.com>
2025-01-23 17:29:28 +0300

    Initial commit

---
+++ src/pages/500.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Custom500() {
+    return (
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+        <h1>500 - Server-side error occurred</h1>
+      </div>
+    );
+  }
+  
